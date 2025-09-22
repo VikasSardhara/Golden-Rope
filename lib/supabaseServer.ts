8fc2b4398444f164cf72dc05d9238404a709d0a9
Vikas Sardhara <75750319+VikasSardhara@users.noreply.github.com>
2025-09-21 17:34:51 -0700

    Create supabaseServer.ts

---
+++ lib/supabaseServer.ts
@@ -0,0 +1,10 @@
+import "server-only";
+import { createClient } from "@supabase/supabase-js";
+
+export function supabaseServer() {
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const serviceKey = process.env.SUPABASE_SERVICE_KEY!;
+  return createClient(url, serviceKey, {
+    auth: { persistSession: false, autoRefreshToken: false },
+  });
+}
